b21c0f9a68c33cd157ea51292e306d0cad2bd070
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2025-06-05 22:31:35 -0400

    fix: usar db push en lugar de migrate para producción

--- backend/prisma/migrations/20250521140951_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Producto" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "nombre" TEXT NOT NULL,
-    "precio" REAL NOT NULL,
-    "stock" INTEGER NOT NULL
-);
