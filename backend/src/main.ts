9f374664865a7f5602a445a856561a0542cd051f
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2025-06-05 20:15:57 -0400

    feat: habilitar CORS y configurar DATABASE_URL para producción

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Habilitar CORS para permitir llamadas desde el frontend
   app.enableCors({
-    origin: "http://localhost:3000", // o "*"
+    origin: '*',
   });
   await app.listen(process.env.PORT ?? 3001);
 }
